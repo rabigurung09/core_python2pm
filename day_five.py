9d997e47d1b0accb8413e0e83ac49bad9025ae3f
rabigurung09 <rabi72732@gmail.com>
2026-06-04 13:06:09 +0545

    my pratice

---
+++ day_five.py
@@ -0,0 +1,71 @@
+# ====case_conversation method====
+p_n="hello world"
+print(p_n.upper())
+# HELLO WORLD
+programing_l="PYTHON PROGRAMING"
+print(programing_l.lower())
+# python programing
+s="hello world"
+print(s.capitalize())
+# Hello world
+# Q4.Differentiate between title() and capitalize() with example.
+"""
+     difference between title and capitalizer is that capatilizer only capitalize the first 
+word of value while making all word small and title makes the value of first word of every value capitalize
+    """
+
+name="rabi gurung"
+print(name.title()) 
+# Rabi Gurung
+name_1="rabindra"
+print(name_1.capitalize())
+# Rabindra
+# Q5.Write a program to capitalize only the first letter of the string "python is fun".
+pro_name="python is fun"
+print(pro_name.capitalize())
+# Python is fun
+# Q6.What will be the output?
+s = "hELLO wORLD"
+print(s.capitalize())
+# Hello world
+"""
+Q7.Write a program to take user input as (name,collegeName,address,favouritePlayer,FavouriteGame) and convert it into:
+    •	uppercase 
+    •	lowercase 
+    
+    """
+user_1=input("Enter your college name=")
+user_2=input("Enter your address=")
+user_3=input("Enter your favourateplayer=")
+user_4=input("Enter your favourategame=")
+print(user_1.upper())
+print(user_2.upper())
+print(user_3.upper())
+print(user_4.upper())
+# UNIVERSAL
+# BHAKTAPUR
+# NEMAR
+# FOOTBALL
+"""Q8.What will be the output?
+    s = "123abc"
+    print(s.upper())"""
+s = "123abc"
+print(s.upper())
+# 123ABC
+"""Q9.Correct the following string using proper case (title case):
+    s = "welcome to nepal"
+    """
+s="welcome to nepal"
+print(s.title())
+# Welcome To Nepal
+# Q10.Predict the output:
+"""    s = "python programming"
+    print(s.capitalize())
+    print(s.title())
+"""
+# Python programing
+# Python Programing
+
+
+
+
